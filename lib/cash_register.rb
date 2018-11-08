3fec4b2363bff29cb29a57243f2d21e66c636c4b
Cindy Pallares <cindy.pallaresq@gmail.com>
2018-11-08 02:56:44 +0000

    automatically backed up by learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,9 +1,12 @@
 class CashRegister
 
   attr_accessor(:discount, :total)
+  
   def initialize(discount=0)
     @total = 0
     @discount = discount
   end
 
+  def add_item(title, price)
+    @total += price
 end
